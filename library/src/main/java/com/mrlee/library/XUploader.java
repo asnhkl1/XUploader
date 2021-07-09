03ddfbe11b66c724fc45a23ae68bd58e927f9364
Mr.Lee <120124135@qq.com>
2021-07-09 13:31:53 +0800

    fix

---
+++ library/src/main/java/com/mrlee/library/XUploader.java
@@ -0,0 +1,17 @@
+package com.mrlee.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class XUploader extends ViewGroup {
+
+    public XUploader(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
